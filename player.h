a710b61ca2edd58310542262a2b444ee792eb852
ccamilomotta-crypto <ccamilomotta@gmail.com>
2026-04-29 23:58:03 -0500

    Add player.h header file for Player struct

---
+++ player.h
@@ -0,0 +1,12 @@
+// this file is provided.  do not modify. do not submit.
+#ifndef PROVIDED_PLAYER_H_
+#define PROVIDED_PLAYER_H_
+
+// Player object container
+struct Player {
+  int row = 0;
+  int col = 0;
+  int treasure = 0;
+};
+
+#endif  // PROVIDED_PLAYER_H_
